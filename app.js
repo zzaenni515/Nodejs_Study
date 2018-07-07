0195c0a9f4dbba734a3a2bd321d3bd5a1f90640e
zzaenni515 <zzaenni515@naver.com>
2018-07-07 15:49:41 +0900

    Express, URL을 이용한 정보 전달 (쿼리스트링)

--- app.js
+++ app.js
@@ -4,6 +4,23 @@ app.locals.pretty = true;
 app.set('view engine', 'jade');
 app.set('views', './views');
 app.use(express.static('public'));//정적 파일 서비스 하는 법
+app.get('/topic', function(req, res){
+  var topics = [
+    'Javascript is...',
+    'Nodejs is ...',
+    'Express is ...'
+  ];
+  var output = `
+    <a href="/topic?id=0">JavaScript</a><br>
+    <a href="/topic?id=1">Nodejs</a><br>
+    <a href="/topic?id=2">Express</a><br><br>
+    ${topics[req.query.id]}
+  `
+  res.send(output);
+});
+app.get('/param/:module_id/:topic_id', function(req, res){
+  res.json(req.params);
+});
 app.get('/template', function(req,res){
   res.render('temp', {time:Date(), title:'Jade'});
 });//temp파일 웹페이지로 렌더링 해서 전송(변수 주입하고싶으면 {}객체 형태로 주입시키면 됨
