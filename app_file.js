24c6c83e53484c5f547661d32dfd615bd3e550ef
zzaenni515 <zzaenni515@naver.com>
2018-07-09 01:28:22 +0900

    node.js 를 이용한 웹앱 제작 실습- 코드개선(중복코드 제거), supervisor 설치

--- app_file.js
+++ app_file.js
@@ -1,3 +1,4 @@
+//supervisor 설치(자동컴파일->실행됨 편함 굿굿)
 var express = require('express');
 var bodyParser = require('body-parser');
 var fs = require('fs');
@@ -7,33 +8,35 @@ app.locals.pretty = true;//줄바꿈 이쁘게 해줌
 app.set('views', './views_file');
 app.set('view engine', 'jade');
 app.get('/topic/new', function(req, res){
-  res.render('new');
-});
-app.get('/topic', function(req, res){
   fs.readdir('data', function(err, files){
     if(err){
       console.log(err);
       res.status(500).send('Internal Server Error');
     }
-    res.render('view', {topics:files});
+  res.render('new', {topics:files});
+  });
+});
+app.get(['/topic', '/topic/:id'], function(req, res){
+  fs.readdir('data', function(err, files){
+    if(err){
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    }
+    var id = req.params.id;
+    if(id){//id 값이 있을 때
+      fs.readFile('data/'+id, 'utf8', function(err, data){
+        if(err){
+          console.log(err);
+          res.status(500).send('Internal Server Error');
+        }
+        res.render('view', {topics:files, title:id, description:data});
+      });
+    }
+    else{//id가 없을 때
+      res.render('view', {topics:files, title:'Welcome', description:'Hello, JavaScript for server.' });
+    }
   });
 });//글 불러와 읽기
-app.get('/topic/:id', function(req, res){
-  var id = req.params.id;
-  fs.readdir('data', function(err, files){
-    if(err){
-      console.log(err);
-      res.status(500).send('Internal Server Error');
-    }
-  fs.readFile('data/'+id, 'utf8', function(err, data){
-    if(err){
-      console.log(err);
-      res.status(500).send('Internal Server Error');
-    }
-    res.render('view', {topics:files, title:id, description:data});
-  });//글 목록을 가져오고,
-});
-});
 app.post('/topic', function(req, res){//글저장
   var title = req.body.title;
   var description = req.body.description;
@@ -42,7 +45,8 @@ app.post('/topic', function(req, res){//글저장
       console.log(err);//error 상세 내용을 뿌려줌
       res.status(500).send('Internal Server Error');
     }
-      res.send('Success!');
+    //  res.send('Success!');
+      res.redirect('/topic/'+title);//작성한 글의 상세보기 페이지로 리다이렉트
   });
 });
 app.listen(3000,function(){
